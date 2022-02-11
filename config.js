3d2e9d595ea85655325e69f56276f32b63923f3a
uybugbuybygu <99462599+uybugbuybygu@users.noreply.github.com>
2022-02-11 18:43:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiên",
-    introTitle: 'Tiên',
+    titleWeb: "Bảo",
+    introTitle: '...',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
