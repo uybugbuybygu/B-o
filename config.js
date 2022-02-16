d7ec6a3110491b235414e79cb7815e7f07686072
MasterThien <94881701+MasterThien@users.noreply.github.com>
2022-02-16 21:14:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Gửi vợ tương lai",
-    introTitle: 'Anh Thư',
+    introTitle: 'Đinh T Hồng Hạnh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Click vào để tiếp tục',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    title: 'Mày có thích tao không???',
+    desc: 'Chọn A hoặc B ',
+    btnYes: 'A.Thích lắm <33',
+    btnNo: 'B.Đéo:3',
+    question: 'Mày thích ai nhất, ghi vào <3',
+    btnReply: 'Gửi bạn Thiện đẹp trai<3',
     reply: 'Thiện Thiện Thiện',
     mess: 'Tui biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Làm người iu anh nhé beby.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/AnhThuLaCrushCuaTui ' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/Hong.Tri.Thien ' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
