38badffce753f08ce453d05ed3fb138a24103c0c
uybugbuybygu <99462599+uybugbuybygu@users.noreply.github.com>
2022-02-28 20:15:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Gửi vợ tương lai",
-    introTitle: 'Đinh T Hồng Hạnh',
+    introTitle: 'Phan L Thủy Tiên',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Click vào để tiếp tục',
-    title: 'Mày có thích tao không???',
+    title: 'Bạn có thích mình không???',
     desc: 'Chọn A hoặc B ',
     btnYes: 'A.Thích lắm <33',
-    btnNo: 'B.Đéo:3',
-    question: 'Mày thích ai nhất, ghi vào <3',
-    btnReply: 'Gửi bạn Thiện đẹp trai<3',
-    reply: 'Thiện Thiện Thiện',
+    btnNo: 'B.Không:3',
+    question: 'Bạn thích ai nhất, ghi vào <3',
+    btnReply: 'Gửi bạn Bảo đẹp trai<3',
+    reply: 'Bảo Bảo Bảo',
     mess: 'Tui biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Làm người iu anh nhé beby.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/Hong.Tri.Thien ' //link mess của các bạn. VD: https://m.me/nam.nodemy
+  
 }
